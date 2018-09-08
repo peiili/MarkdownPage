7778c23dce381bb5518b4a8403e39b7944625075
peiili <1017935195@qq.com>
2018-09-09 01:29:25 +0800

    后台路由设置完成

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs = require("fs");
+
 const Router = express.Router();
 
-
-
-
-
+//管理静态资源
+app.use(express.static(path.join(__dirname,'static')));
 //集成路由,使用路由的方法;
 const infoRouter = require(path.join(__dirname,"./router/router.js"));
 //使用路由中的方法;
+app.use('/md',infoRouter);
 
 
 //开启web服务
--- router/router.js
+++ router/router.js
@@ -1,15 +1,32 @@
 const path = require("path");
 const express =require("express");
 const infoRouter = express.Router(); 
+const fs = require("fs");
 //导入
 const markdown = require( "markdown" ).markdown;
 
-infoRouter.get("/",(req,res)=>{
+//获取首页
+infoRouter.get("/index",(req,res)=>{
+    //使用模板
+    
+   fs.readFile(path.join(__dirname,"../static/page/index.html"),(err,data)=>{
+        res.send(data.toString());
+    });
+
+  
+});
+//获取Md子页面
+infoRouter.get("/test",(req,res)=>{
+
     //异步获取md文件;
-    fs.readFile(path.join(__dirname,"test.md"),(err,data)=>{
+    fs.readFile(path.join(__dirname,"../static/mdFiles/test.md"),(err,data)=>{
     //将原始的数据进行解析,
+   // console.log(data);
      const md = data.toString();
      //将数据进行解析为html,并返回给浏览器
      res.send( markdown.toHTML(md));
     });
  });
+
+ //将方法暴露出去
+ module.exports = infoRouter;
